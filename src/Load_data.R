17cd91a95c3628e7f54d570f5b659ba9a03da97a
Paul Vial <vial.paul@gmail.com>
2018-11-22 15:01:34 -0800

    added drafts for scripts 3 and 4

--- src/Load_data.R
+++ src/Load_data.R
@@ -1,8 +1,28 @@
-# Required packages
+#! /usr/bin/env Rscript 
+# quick_load_data.R
+# Socorro Dominguez, 
+# Paul Vial 
+# November 2018
+#
+#
+# This script inputs the `Top Tracks of 2017.csv` and gives out a tidy form of data.
+# variable from a .csv file. This script takes a filename.
+#
+# Usage: Rscript load_data.R ../data/top-tracks-of-2017.csv ../data/clean_top-tracks-of-2017.csv
 
+# read in command line arguments
 library(tidyverse)
+args <- commandArgs(trailingOnly = TRUE)
+input_file <- args[1]
+output_file <- args[2]
 
-# Loading data of the Top Tracks in Spotify and the Daily Ranking.
 
-daily_ranking <- read_csv("data/spotify-worldwide-daily-song-ranking.csv")
-top_tracks <- read_csv("data/top-tracks-of-2017.csv")
+# read in data
+main <- function(){ 
+  data <- read_csv(input_file)
+  clean_data <- write_csv(data, output_file)
+
+}  
+
+# call main function
+main()
