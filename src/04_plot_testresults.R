99e79402d20c2cb34f559014788c7c9de416a85e
Socorro <sedv8808@gmail.com>
2018-11-24 12:59:57 -0800

    Update plot

--- src/04_plot_testresults.R
+++ src/04_plot_testresults.R
@@ -12,30 +12,25 @@
 # This script simulates data under a model of the null hypothesis and generates a plot of the distribution which includes
 # the confidence interval and test statistic from the sample population.  It writes the plot to a file in PNG format.
 #
-# This script takes four arguments: a path/filename pointing to the data, a path/filename pointing to the summarized data statistics, and
-# 2 x path/filename prefix where to write the plots to and what to call them.
+# This script takes three arguments: a path/filename pointing to the data, a path/filename pointing to the summarized data statistics, and
+# a path/filename prefix where to write the plot to and what to call it.
 #
-# Usage: 
-# Rscript src/04_plot_testresults.R ./data/clean_top_tracks.csv ./data/summary_data.csv ./results/figure/Fig03_Test_Ddistr_Plot.png ./results/figure/Fig04_Sample_Compare_Plot.png
+# Usage: Rscript src/04_plot_testresults.R ./data/clean_top_tracks.csv ./data/summary_data.csv ./results/figure/Fig03_Test_Ddistr_Plot.png
 
 library(tidyverse)
 library(infer)
 
 # read in command line arguments
 args <- commandArgs(trailingOnly = TRUE)
-data_input_file <- args[1] #"./data/clean_top_tracks.csv"
-summary_input_file <- args[2] #"./data/summary_data.csv"
-output_file_1 <- args[3] #"./results/figure/Fig03_Test_Ddistr_Plot.png"
-output_file_2 <- args[4] #"./results/figure/Fig04_Sample_Compare_Plot.png"
+data_input_file <- args[1]
+summary_input_file <- args[2]
+output_file <- args[3]
 
 main <- function(){ 
   
   # read in data
   data <- read_csv(data_input_file)
   summary <- read_csv(summary_input_file)
-  
-  # set threshold
-  alpha = 0.1
   
   # make rank and mode a factor
   mode_rank <- data %>% 
@@ -45,35 +40,14 @@ main <- function(){
   # and calculate the test statistic for each simulated sample
   set.seed(123)
   null_dist <- mode_rank %>% 
+    # filter(mode == 0) %>% 
     specify(response = rank, explanatory = mmode) %>% 
     hypothesize(null = "independence") %>% 
-    generate(reps = 10000, type = "bootstrap") %>% 
+    generate(reps = 10000, type = "permute") %>% 
     calculate(stat = "diff in means", order = c("0", "1"))
   
-  # Get confidence intervals for each key-mode
-  m0_ci <- mode_rank %>% 
-    filter(mmode == 0) %>% 
-    specify(response = rank) %>% 
-    generate(reps = 10000, type = "bootstrap") %>% 
-    calculate(stat = "mean") %>% 
-    get_ci(level = (1 - alpha))
-  m0_ci$mmode <- 0
-  
-  m1_ci <- mode_rank %>% 
-    filter(mmode == 1) %>% 
-    specify(response = rank) %>% 
-    generate(reps = 10000, type = "bootstrap") %>% 
-    calculate(stat = "mean") %>% 
-    get_ci(level = (1 - alpha))
-  m1_ci$mmode <- 1
-  
-  # Add the confidence intervals to the summary table
-  mode_means_CIs <- bind_rows(m0_ci, m1_ci)
-  summary <- left_join(summary, mode_means_CIs)
-  colnames(summary) <- c("key_mode", "average_rank", "count", "diff_estimate", "lower_ci", "upper_ci")
-  
-  # Calculate confidence interval for diffeence in means
-  (threshold <- quantile(null_dist$stat, c(alpha, 1-alpha)))
+  # Calculate confidence interval
+  (threshold <- quantile(null_dist$stat, c(0.1, 0.9)))
   
   # Extract the original sample's test statistic from the summary input file
   rank_diff_estimate <- summary[['diff_estimate']][[1]]
@@ -82,8 +56,9 @@ main <- function(){
   p_value <- null_dist %>% 
     get_pvalue(obs_stat = rank_diff_estimate, direction = "two_sided")
   
+  
   # write the following plot to output file
-  # png(output_file_1)
+  png(output_file)
   
   # visualize the null distribution with confidence interval and the test statistic from the original sample
   (null_dist_plot <- null_dist %>% visualize()) +
@@ -92,28 +67,8 @@ main <- function(){
                color = "blue",
                lty = 2) +
     geom_vline(xintercept = rank_diff_estimate, color = "red") +
-    xlab("difference in mean song ranking between key-modes")
-  
-  # dev.off()
-  
-  # write the following plot to output file
-  # png(output_file_2)
-  ggsave(output_file_1)
-  
-  # visualize the estimates and key-mode ranking means' ci's side by side
-  ggplot(summary, aes(x = key_mode, y = average_rank)) +
-      geom_point() +
-      geom_jitter(aes(x = mmode, y = rank, color = as.character(mmode)), data = data, width = 0.2) +
-      geom_errorbar(aes(ymin = lower_ci, ymax = upper_ci), width = 0.1) +
-      xlab("Song Key-mode") +
-      ylab("Ranking") + 
-      # scale_x_discrete(labels = c("Minor", "Major")) +
-      scale_fill_discrete(name="Key-mode",
-                        breaks=c("Minor", "Major"),
-                        labels=c("Minor", "Major")) +
-      theme_bw()
-  
-  ggsave(output_file_2)
+    xlab("Difference in mean song ranking between key-modes") +
+    annotate("text", x = 15, y = 1500, label = paste("P-Val = ", p_value))
 }
 
 # call main function
